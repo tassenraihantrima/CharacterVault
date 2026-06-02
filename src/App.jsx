bd05da253149c45e2281b7ebe1d6027919194eb3
Tassen Raihan Trima <tassenraihantrima@gmail.com>
2026-06-02 13:35:54 -0600

    Initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>CharacterVault</h1>
+
+      <h2>My Novels</h2>
+
+      <button>Create Novel</button>
+
+      <ul>
+        <li>Novel 1</li>
+        <li>Novel 2</li>
+        <li>Novel 3</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
